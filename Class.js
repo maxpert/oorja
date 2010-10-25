02d45722768cbecf2a4a6e229be010cbb6428d36
Zohaib Sibt-e-Hassan <zohaib.hassan@gmail.com>
2010-10-25 23:57:20 +0500

    first official commit

--- Class.js
+++ Class.js
@@ -1,3 +1,27 @@
+/*
+The MIT License
+
+Copyright (c) 2010 Zohaib Sibt-e-Hassan(MaXPert)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 var Class = function(){
     var slice = Array.prototype.slice,
         args = slice.call(arguments),
@@ -85,8 +109,6 @@ var Class = function(){
 		
 		//Create a VTable including publics and privates
 		var vis = {};
-		merge(vis, privates);
-		merge(vis, this);
 		
 		//Create VTable call for function
 		var vtablize = function(func){
@@ -108,12 +130,14 @@ var Class = function(){
 		//Overide inherted vtabled
 		for(i in publics){
 			if(publics.hasOwnProperty(i) && typeof publics[i] == 'function'){
-				this[i] = vtablize(publics[i]);
+				klass.prototype[i] = vtablize(publics[i]);
 			}else if(publics.hasOwnProperty(i)){
 				this[i] = publics[i];
 			}
 		}
 		
+		merge(vis, privates);
+		merge(vis, this);
 		
 		init.apply(this, arguments);
 	};
